c2e83b55fe8d35a19fbf23dc864c42c8d9d1e1e0
Dan Timmins <dantimmins97@gmail.com>
2024-04-06 12:53:01 -0400

    Basic horizontal movement added OnMove / Run

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -6,21 +7,29 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] float runSpeed = 5f;
     Vector2 moveInput;
+    Rigidbody2D playerRigidbody;
 
-    void Start()
+    void Awake()
     {
-        
+        playerRigidbody = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        
+        Run();
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, playerRigidbody.velocity.y);
+        playerRigidbody.velocity = playerVelocity;
     }
 
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
-        Debug.Log(moveInput);
+        //Debug.Log(moveInput);
     }
 }
