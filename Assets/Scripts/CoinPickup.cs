1ca90d4193433956cde3451e1c621c106f6debc1
Dan Timmins <dantimmins97@gmail.com>
2024-04-15 19:13:08 -0400

    Coins now make a sound on pickup.

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
+    [SerializeField] AudioClip coinPickupSfx;
     void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.tag == "Player")
         {
+            AudioSource.PlayClipAtPoint(coinPickupSfx, Camera.main.transform.position);
             Destroy(gameObject);
         }    
     }
